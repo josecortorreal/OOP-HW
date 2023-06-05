beb1e1ffdaa336feba8fd55cee70054ca1908b6b
josecortorreal <josebibby23@gmail.com>
2023-06-04 23:24:37 -0400

    added an svg.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const readline = require('readline');
 const fs = require('fs');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const SVG = require('svg.js');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,42 +8,48 @@ const rl = readline.createInterface({
 });
 
 function promptUser(question) {
-    return new Promise((resolve) => {
-      rl.question(question, resolve);
-    });
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
+  });
+}
+
+async function generateLogo() {
+  const text = await promptUser('Enter up to three characters: ');
+  const textColor = await promptUser('Enter the text color (keyword or hexadecimal): ');
+  const shape = await promptUser('Choose a shape (circle, triangle, square): ');
+  const shapeColor = await promptUser('Enter the shape color (keyword or hexadecimal): ');
+
+  const svg = SVG().size(300, 200);
+  
+  // Draw the shape
+  switch (shape.toLowerCase()) {
+    case 'circle':
+      svg.circle(200).fill(shapeColor);
+      break;
+    case 'triangle':
+      svg.polygon('0,200 100,0 200,200').fill(shapeColor);
+      break;
+    case 'square':
+      svg.rect(200, 200).fill(shapeColor);
+      break;
+    default:
+      console.log('Invalid shape choice. Exiting...');
+      rl.close();
+      return;
   }
-  
-  async function generateLogo() {
-    const text = await promptUser('Enter up to three characters: ');
-    const textColor = await promptUser('Enter the text color (keyword or hexadecimal): ');
-    const shape = await promptUser('Choose a shape (circle, triangle, square): ');
-    const shapeColor = await promptUser('Enter the shape color (keyword or hexadecimal): ');
-  
-    let logoShape;
-    switch (shape.toLowerCase()) {
-      case 'circle':
-        logoShape = new Circle(shapeColor);
-        break;
-      case 'triangle':
-        logoShape = new Triangle(shapeColor);
-        break;
-      case 'square':
-        logoShape = new Square(shapeColor);
-        break;
-      default:
-        console.log('Invalid shape choice. Exiting...');
-        rl.close();
-        return;
-    }
 
-    const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-      <rect width="100%" height="100%" fill="${logoShape.color}" />
-      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-size="48">${text}</text>
-    </svg>
-  `;
+  svg.circle(150)
+    .fill('green')
+    .center(150, 100);
 
-  fs.writeFile('logo.svg', svgContent, (err) => {
+
+  // Draw the text
+  svg.text(text)
+    .fill(textColor)
+    .font({ size: 48 })
+    .center(150, 100);
+
+  fs.writeFile('logo.svg', svg.svg(), (err) => {
     if (err) {
       console.log('An error occurred while saving the SVG file.');
     } else {
@@ -52,5 +58,6 @@ function promptUser(question) {
     rl.close();
   });
 }
+
 
 generateLogo();
